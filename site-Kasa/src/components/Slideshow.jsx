78fbff839e9bedfce8c197031b2899fdf51211f3
InesGALLIERO <104718756+InesGALLIERO@users.noreply.github.com>
2026-03-25 20:44:18 +0100

    collapse

--- site-Kasa/src/components/Slideshow.jsx
+++ site-Kasa/src/components/Slideshow.jsx
@@ -3,19 +3,43 @@ import Arrow from "../assets/Arrow.svg"
 import "../styles/Slideshow.scss"
 
 function Slideshow ({pictures}) {
+
+    //indexActuel = position actuelle dans le tableau 
+    // ex: 0 = premiere image, 1 = deuxieme, ect
     const [indexActuel, setIndexActuel] = useState(0);
 
     const droite = () => {
+        
+        // CONDITION :
+        // on vérifie si on est sur la DERNIÈRE image
+        // pictures.length - 1 = index de la dernière image
+
         if(indexActuel === pictures.length - 1){
+
+            // CAS 1 : on est à la fin
+            // → on "boucle" → on revient au début 
             setIndexActuel(0)
+
         }else{
+
+            // CAS 2 : on n'est PAS à la fin
+            // → on avance simplement d'une image
             setIndexActuel(indexActuel + 1);
         }
     };
     const gauche = () => {
+
+        // CONDITION :
+        // on vérifie si on est sur la PREMIÈRE image
         if(indexActuel === 0){
+
+            // CAS 1 : on est au début
+            // → on "boucle" vers la fin
             setIndexActuel(pictures.length - 1);
         }else {
+
+            // CAS 2 : on n'est PAS au début
+            // → on recule d'une image
             setIndexActuel(indexActuel - 1);
         }
     };
@@ -25,11 +49,23 @@ function Slideshow ({pictures}) {
 
             <img 
                 className="slideshow__image"
+
+                // on affiche l'image correspondant à l'index actuel
+                // => accès au tableau avec une position dynamique
                 src = {pictures[indexActuel]}
+
+                // +1 car index commence à 0 mais utilisateur voit 1
                 alt={`slide ${indexActuel + 1}`}
             />
+          {/* 
+        CONDITION D'AFFICHAGE (render conditionnel)
 
-        
+        pictures.length > 1 signifie :
+        → "est-ce qu'il y a plus d'une image ?"
+
+        Si TRUE → on affiche les flèches + compteur
+        Si FALSE → on n'affiche rien (inutile s'il y a 1 seule image)
+        */}
         
         {pictures.length > 1 && (
             <>
